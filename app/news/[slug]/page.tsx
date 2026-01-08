ee6c6039ad4855e84d1991ec1a896348c40d268d
bil022 <bil022@ucsd.edu>
2026-01-07 16:23:27 -0800

    page.tsx images

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -43,7 +43,7 @@ export default async function NewsPostPage({ params }: PageProps) {
 
           {post.image && post.image.title && (
             <img
-              src={`/images/${post.image.title}`}
+              src={`images/${post.image.title}`}
               alt={post.image.title}
               style={{ maxWidth: '100%', height: 'auto', marginBottom: '1rem' }}
             />
